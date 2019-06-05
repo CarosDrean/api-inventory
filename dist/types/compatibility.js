21385d11e1796a957414bd999f8f884b737ccc1f
Oscar Salcedo <josueht15@gmail.com>
2019-06-05 12:11:20 -0500

    eliminado dist

--- dist/types/compatibility.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = "\n    type Compatibility {\n        _id: ID\n        name: String!\n    }\n\n    input CompatibilityInput {\n        name: String!\n    }\n\n    type Query {\n        compatibility(_id: ID!): Compatibility\n        compatibilitys: [Compatibility]\n    }\n\n    type Mutation {\n        createCompatibility(input: CompatibilityInput): Compatibility\n        deleteCompatibility(_id: ID): Compatibility\n        updateCompatibility(_id: ID, input: CompatibilityInput): Compatibility\n    }\n";
-exports["default"] = _default;
-//# sourceMappingURL=compatibility.js.map
--- dist/types/product.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = "\n    type Product {\n        _id: ID\n        name: String!\n        category: String\n        model: String\n        description: String\n        trademark: String\n        measure: String\n        color: String\n        stock: Int\n        price: Float\n        compatibility: String\n    }\n\n    input ProductInput {\n        name: String!\n        category: String\n        model: String\n        description: String\n        trademark: String\n        measure: String\n        color: String\n        stock: Int\n        price: Float\n        compatibility: String\n    }\n\n    type Query {\n        product(_id: ID!): Product\n        products: [Product]\n    }\n\n    type Mutation {\n        createProduct(input: ProductInput): Product\n        deleteProduct(_id: ID): Product\n        updateProduct(_id: ID, input: ProductInput): Product\n    }\n";
-exports["default"] = _default;
-//# sourceMappingURL=product.js.map
